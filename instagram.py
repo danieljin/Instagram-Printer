9fa30dabff15fb924da05aa4e6b5c32eeeaffbb3
Daniel Jin <danilekjin@gmail.com>
2015-09-09 02:21:55 +0000

    improve template.py and move it into main file

--- instagram.py
+++ instagram.py
@@ -2,7 +2,7 @@ import urllib2
 import time
 import json
 import subprocess
-import os
+import os, shutil, shutil
 startTime = int(time.time())
 polling_interval = 10
 running = True
@@ -16,14 +16,14 @@ def poll_instagram(): #program does nothing as written
     json_data = json.loads(response)
     for post in json_data['data']:
         if instagram_tag in post["caption"]["text"]:
-            # if int(post["caption"]["created_time"]) > startTime:
-            if post["id"] not in photosDict:
-                photosDict[post["id"]] = post["images"]["standard_resolution"]["url"]
-        for comment in post["comments"]["data"]:
-            if instagram_tag in comment["text"]:
-                # if int(comment["created_time"]) > startTime:
+            if int(post["caption"]["created_time"]) > startTime:
                 if post["id"] not in photosDict:
                     photosDict[post["id"]] = post["images"]["standard_resolution"]["url"]
+        for comment in post["comments"]["data"]:
+            if instagram_tag in comment["text"]:
+                if int(comment["created_time"]) > startTime:
+                    if post["id"] not in photosDict:
+                        photosDict[post["id"]] = post["images"]["standard_resolution"]["url"]
 def print_photos():
     for id, url in photosDict.iteritems():
         if id not in printedIds:
@@ -45,10 +45,43 @@ def print_photos():
             print filename+" has been printed."
             running = True
 
+def apply_template(input_filename, output_filename, username, caption):
+    mkdir_p("tmp")
+    subprocess.Popen(["convert", input_filename, "-resize", "1000x1000", "-gravity", "center", "-extent", "1000x1000", "tmp/resize.JPG"]).communicate()
+    subprocess.Popen(["convert", "template.JPG", "tmp/resize.JPG", "-geometry", "+0+150", "-composite", "tmp/combined.JPG"]).communicate()
+    subprocess.Popen(["convert", "tmp/combined.JPG", "-pointsize", "45", "-fill", "rgb(18,86,136)", "-font", "Helvetica-Bold", "-annotate", "+134+63", username, "tmp/text1.JPG"]).communicate()
+    subprocess.Popen(["convert", "tmp/text1.JPG", "-pointsize", "45", "-fill", "rgb(18,86,136)", "-font", "Helvetica-Bold", "-annotate", "+34+1267", username, "tmp/text2.JPG"]).communicate()
+    caption = insert_new_lines(caption, 45, 0)
+    subprocess.Popen(["convert", "tmp/text2.JPG", "-pointsize", "45", "-fill", "black", "-font", "Helvetica", "-weight", "200", "-annotate", "+34+1322", caption, output_filename]).communicate()
+    print "Image has been converted."
+    shutil.rmtree("tmp")
+
+def insert_new_lines(text, line_length, count):
+    if len(text) <= line_length :
+        return text
+    else :
+        k = text[:line_length].rfind(" ")
+
+        if k < 0 :
+            return text[:line_length] + "\n" + insert_new_lines(text[line_length:], line_length, count + 1)
+        elif count > 2 :
+            return text[:k] + ' ...'
+        else :
+            return text[:k] + "\n" + insert_new_lines(text[k+1:], line_length, count + 1)
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
+
 while running:
     start= time.clock()
     poll_instagram()
     print_photos()
+    print photosDict
     print "tick"
     work_duration = time.clock() - start
     time.sleep( polling_interval - work_duration )
